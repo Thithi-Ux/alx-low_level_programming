3d17c70c621944787bce591c8b9990cfc56a027e
Thithi-Ux <morrinthithi2@gmail.com>
2022-12-08 11:26:07 -0800

    A c program using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,13 @@
 /*
  * main - Entry Point
  *
+ * Description: A C program using printf
+ *
  * Return 0
  *
  */
 
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
