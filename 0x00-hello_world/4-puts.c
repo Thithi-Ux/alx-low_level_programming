0fd35539ec3a365151538977a19ee1a29620be00
Thithi-Ux <morrinthithi2@gmail.com>
2022-12-08 12:21:05 -0800

    A C progrma for a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
+ * 
+ * Description: A C program that uses output Puts
  *
- * return value of 0
+ * Return: Always 0 (Success)
  *
  */
 
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
 
