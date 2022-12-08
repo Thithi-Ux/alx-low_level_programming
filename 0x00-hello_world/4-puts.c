a277d3be54c71a971fa00ecc4bb06466e7d99e23
Thithi-Ux <morrinthithi2@gmail.com>
2022-12-08 11:09:27 -0800

    Prints text followed by new line and should return value 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * return value of 0
+ *
+ */
 
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-		return(0);
+	return(0);
 }
 
