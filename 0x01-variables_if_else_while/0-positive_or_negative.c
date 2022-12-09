841184148a288be96a8c232e5e608903346cedcb
Thithi-Ux <morrinthithi2@gmail.com>
2022-12-09 06:22:29 -0800

    A C progrm using else if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 /**
  * main - Entry point
  * Description: A C program that assigns a random number to a variable
@@ -14,16 +13,20 @@ int main(void)
 
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 		
 	/* your code goes there */
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	
+	else if (n < 0);
+		printf("%d is negative\n", n);
+	
 	else (n < 0)
 		printf("%d is negative\n", n);
+	
 	return (0);
 
 }
