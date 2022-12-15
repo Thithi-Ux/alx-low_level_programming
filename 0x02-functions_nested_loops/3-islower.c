40a8e37d781d49c24b2309b40230aea4ca4c8900
Thithi-Ux <morrinthithi2@gmail.com>
2022-12-15 08:54:44 -0800

    a function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int_islower(int c) - Entry point
+ * description a function that checks for lowercase character
+ * Return (0) Success
+ *
+ */
+
+int _islower(int c)
+{
+	if ( c >= 97 && c <= 122 )
+	{
+		return(1);
+	}
+	return(0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
+void print_int_islower(int c);
 
 #endif
