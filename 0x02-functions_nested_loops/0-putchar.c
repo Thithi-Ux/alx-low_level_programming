b7bcd82e7d8c7b88ef4969e11b7c69b563eacdd0
Thithi-Ux <morrinthithi2@gmail.com>
2022-12-14 17:50:02 -0800

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * description- a program that prints _putchar, followed by a new line.
+ *
+ * Retun 0 (Always)
+ *
+ */
+
+int main(void)
+{
+	char *c = "_putchar"
+		while (*c)
+		{
+			putchar(*c);
+			c++;
+		}
+	_putchar('\n');
+	return (0);
+}
+
