842a296204dfe7719cabe7ce645b74b3f6ee0192
Thithi-Ux <morrinthithi2@gmail.com>
2022-12-20 00:06:26 -0800

    Print both lower and uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- a program that prints lower and upppercase alphabets
+ * description :use putchar function
+ *
+ * Return: (0) success
+ *
+ */
+int main(void)
+{
+	chat c;
+	for (c = 'a', c = 'z', c++)
+		putchar(c);
+	for (c= 'A', c = 'z', c++)
+		putchar(c);
+	putchar('\n');
+	return(0);
+}
