f7c11675d9236b7faac5632170b1374f5f59dcb2
Thithi-Ux <morrinthithi2@gmail.com>
2022-12-21 01:08:51 -0800

    Print alphabets in upper and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,12 @@
 int main(void)
 {
 	char c;
+	
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-	for (c = 'A'; c <= 'z'; c++)
+	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
+	
 	putchar('\n');
 	return(0);
 }
