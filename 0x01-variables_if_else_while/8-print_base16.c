e09083d66d13b038aea1dcd49de10ec068d735d3
Thithi-Ux <morrinthithi2@gmail.com>
2022-12-21 02:10:14 -0800

    Prints numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,8 +13,8 @@ int main(void)
 	int i;
 	char c;
 
-	for (i = 0; i <= 10; i++)
-		putchar((i % 10)+ '0');
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
 
 	for (c = 'a'; c <= 'f'; c++)
 		putchar(c);
